063a65533cbc42879350e7146b9e9c18ed9ed75e
Rocco Panacci <rocco@Roccos-MacBook-Pro.local>
2020-04-02 12:09:18 -0400

    first commit.

---
+++ js/index.js
@@ -0,0 +1,33 @@
+const $show = document.querySelector('.show')
+
+
+// A function that goes to the start of the show
+const setFirstSlide = () => {
+
+}
+
+// A function that goes to the end of the show
+const setLastSlide = () => {
+
+}
+
+// Remove ".current" from all ".slide"
+const unsetSlides = () => {
+
+}
+
+// Previous slide
+const prevSlide = () => {
+
+}
+
+// Next slide
+const nextSlide = () => {
+
+}
+
+// When the interface has fully loaded...
+const windowLoaded = () => {
+
+}
+
